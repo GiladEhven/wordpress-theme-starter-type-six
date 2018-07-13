24e6aa8a1f0194f1dbc71beab212eb5e61679fbd
Gilad Ehven <gilad@ehven.com>
2018-07-13 01:12:38 -0400

    Added minimalist views (one per default WP template) with demo [] array elements for rendering.

---
+++ public/php/views/class-view-date.php
@@ -0,0 +1,14 @@
+<?php
+
+	namespace Ehven\Gilad\WordPress\Themes\Starters\TypeSix;
+
+	class View_Date { public function __construct( $data ) {
+
+	//  --------------------------  DO NOT EDIT ABOVE THIS LINE  --------------------------  //
+
+			echo '<h1>' . $data['sample-one'] . ' ' . $data['sample-two'] . '</h1>';
+			echo '<h2>And the above heading was assembled from two array values passed to a view (to demonstrate use of this theme\'s structure).</h2>';
+
+	//  --------------------------  DO NOT EDIT BELOW THIS LINE  --------------------------  //
+
+	} } // End [__construct] // End [class]
