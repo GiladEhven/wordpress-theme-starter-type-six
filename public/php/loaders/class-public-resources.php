fcd65db4a6e78198f5939188d1b47e9d76e1d352
Gilad Ehven <gilad@ehven.com>
2018-07-22 14:14:28 -0400

    Implemented resource loader in existing class/placeholder (for public side only).

--- public/php/loaders/class-public-resources.php
+++ public/php/loaders/class-public-resources.php
@@ -10,25 +10,48 @@
 
             private $text_domain;
 
+            private $theme_version;
+
             public static $object_counter = 0;
 
             public function __construct() {
 
-                if ( is_admin() ) {
-
-                	//
-
-                } else {
-
-                	//
-
-                }
-
                 self::$object_counter++;
+
+                $this->set_theme_descriptors();
+
+                $this->dequeue_css();
+                $this->dequeue_js();
+                $this->enqueue_css();
+                $this->enqueue_js();
 
             }
 
-            //
+            private function dequeue_css() {
+
+            }
+
+            private function dequeue_js() {
+
+            }
+
+            private function enqueue_css() {
+                add_action( 'wp_enqueue_scripts', function() {
+                    wp_enqueue_style( $this->text_domain, get_stylesheet_directory_uri() . '/public/css/main.css', array(), $this->theme_version, 'all' );
+                });
+            }
+
+            private function enqueue_js() {
+                add_action( 'wp_enqueue_scripts', function() {
+                    wp_enqueue_script( $this->text_domain, get_stylesheet_directory_uri() . '/public/js/main.js', array( 'jquery' ), $this->theme_version, true );
+                });
+            }
+
+            protected function set_theme_descriptors() {
+                $theme_object = wp_get_theme();
+                $this->text_domain = $theme_object->get( 'TextDomain' );
+                $this->theme_version = $theme_object->get( 'Version' );
+            }
 
         }
 
