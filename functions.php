a159cc78da1218294a91b57fbfda7819a4d1bb1f
Gilad Ehven <gilad@ehven.com>
2019-06-07 01:31:41 -0400

    Added placeholder code in 'functions.php' and placeholder files in 'support' for optional supported capabilities.

--- functions.php
+++ functions.php
@@ -6,15 +6,24 @@
 
 
 
-    if ( ! defined( 'GILAD_DISABLE_EMOJI'     ) ) { define( 'GILAD_DISABLE_EMOJI',     true ); }
-    if ( ! defined( 'GILAD_DISABLE_GENERATOR' ) ) { define( 'GILAD_DISABLE_GENERATOR', true ); }
-    if ( ! defined( 'GILAD_DISABLE_RSD'       ) ) { define( 'GILAD_DISABLE_RSD',       true ); }
-    if ( ! defined( 'GILAD_DISABLE_SHORTLINK' ) ) { define( 'GILAD_DISABLE_SHORTLINK', true ); }
-    if ( ! defined( 'GILAD_DISABLE_WLW'       ) ) { define( 'GILAD_DISABLE_WLW',       true ); }
+    if ( ! defined( 'GILAD_DISABLE_EMOJI'          ) ) { define( 'GILAD_DISABLE_EMOJI',          true );  }
+    if ( ! defined( 'GILAD_DISABLE_GENERATOR'      ) ) { define( 'GILAD_DISABLE_GENERATOR',      true );  }
+    if ( ! defined( 'GILAD_DISABLE_RSD'            ) ) { define( 'GILAD_DISABLE_RSD',            true );  }
+    if ( ! defined( 'GILAD_DISABLE_SHORTLINK'      ) ) { define( 'GILAD_DISABLE_SHORTLINK',      true );  }
+    if ( ! defined( 'GILAD_DISABLE_WLW'            ) ) { define( 'GILAD_DISABLE_WLW',            true );  }
 
 
 
-    if ( ! defined( 'GILAD_GOOGLE_TYPE'       ) ) { define( 'GILAD_GOOGLE_TYPE',       'GA' ); }
+    if ( ! defined( 'GILAD_GOOGLE_TYPE'            ) ) { define( 'GILAD_GOOGLE_TYPE',            'GA' );  }
+
+
+
+    if ( ! defined( 'GILAD_SUPPORT_BEAVER_BUILDER' ) ) { define( 'GILAD_SUPPORT_BEAVER_BUILDER', false ); }
+    if ( ! defined( 'GILAD_SUPPORT_CUSTOMIZER'     ) ) { define( 'GILAD_SUPPORT_CUSTOMIZER',     false ); }
+    if ( ! defined( 'GILAD_SUPPORT_GIVE_WP'        ) ) { define( 'GILAD_SUPPORT_GIVE_WP',        false ); }
+    if ( ! defined( 'GILAD_SUPPORT_GUTENBERG'      ) ) { define( 'GILAD_SUPPORT_GUTENBERG',      false ); }
+    if ( ! defined( 'GILAD_SUPPORT_WOOCOMMERCE'    ) ) { define( 'GILAD_SUPPORT_WOOCOMMERCE',    false ); }
+    if ( ! defined( 'GILAD_SUPPORT_WP_FEATURES'    ) ) { define( 'GILAD_SUPPORT_WP_FEATURES',    false ); }
 
 
 
@@ -22,10 +31,24 @@
 
         require_once( get_stylesheet_directory() . '/functions/admin-cleanup.php' );
 
+        if ( GILAD_SUPPORT_BEAVER_BUILDER ) require_once( get_stylesheet_directory() . '/support/admin-beaver-builder.php' );
+        if ( GILAD_SUPPORT_CUSTOMIZER )     require_once( get_stylesheet_directory() . '/support/admin-customizer.php' );
+        if ( GILAD_SUPPORT_GIVE_WP )        require_once( get_stylesheet_directory() . '/support/admin-give-wp.php' );
+        if ( GILAD_SUPPORT_GUTENBERG )      require_once( get_stylesheet_directory() . '/support/admin-gutenberg.php' );
+        if ( GILAD_SUPPORT_WOOCOMMERCE )    require_once( get_stylesheet_directory() . '/support/admin-woocommerce.php' );
+        if ( GILAD_SUPPORT_WP_FEATURES )    require_once( get_stylesheet_directory() . '/support/admin-wordpress-features.php' );
+
     } else {
 
         require_once( get_stylesheet_directory() . '/functions/public-action-hooks.php' );
         require_once( get_stylesheet_directory() . '/functions/public-cleanup.php' );
+
+        if ( GILAD_SUPPORT_BEAVER_BUILDER ) require_once( get_stylesheet_directory() . '/support/public-beaver-builder.php' );
+        if ( GILAD_SUPPORT_CUSTOMIZER )     require_once( get_stylesheet_directory() . '/support/public-customizer.php' );
+        if ( GILAD_SUPPORT_GIVE_WP )        require_once( get_stylesheet_directory() . '/support/public-give-wp.php' );
+        if ( GILAD_SUPPORT_GUTENBERG )      require_once( get_stylesheet_directory() . '/support/public-gutenberg.php' );
+        if ( GILAD_SUPPORT_WOOCOMMERCE )    require_once( get_stylesheet_directory() . '/support/public-woocommerce.php' );
+        if ( GILAD_SUPPORT_WP_FEATURES )    require_once( get_stylesheet_directory() . '/support/public-wordpress-features.php' );
 
     }
 
---
+++ support/public-beaver-builder.php
@@ -0,0 +1,8 @@
+<?php
+
+	namespace Ehven\Gilad\WordPress\Themes\Starters\TypeSix;
+
+    if ( ! defined( 'ABSPATH' ) ) exit( 'Nothing to see here. Go <a href="/">home</a>.' );
+
+
+
