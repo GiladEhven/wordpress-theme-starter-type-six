43b19a3f74b2af7b0797446e2ef55b9ee467074f
Gilad Ehven <gilad@ehven.com>
2019-06-13 23:56:51 -0400

    Added template part for 'The Loop' used in 'page' type(s).

--- parts/loop-page.php
+++ parts/loop-page.php
@@ -6,3 +6,42 @@
 
 
 
+	if ( have_posts() ) {
+
+		h_loop_if_begin();
+
+		while ( have_posts() ) {
+
+			h_loop_while_begin();
+
+			the_post();
+
+			// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+			h_loop_entry_before();
+
+			get_template_part( 'parts/entry', get_post_type() );
+
+			h_loop_entry_after();
+
+			// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+			h_loop_comments_before();
+
+			if ( comments_open() || get_comments_number() ) comments_template();
+
+			h_loop_comments_after();
+
+			// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+			h_loop_while_end();
+
+		}
+
+		h_loop_if_end();
+
+	} else {
+
+		h_loop_else_core();
+
+	}
