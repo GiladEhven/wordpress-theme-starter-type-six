f53947745bc5b9a15bb3cc99ed7f54502393070d
Gilad Ehven <gilad@ehven.com>
2019-06-13 23:18:04 -0400

    Added template part for 'Entry' used (after 'the_post') in post/single type(s).

---
+++ parts/entry-post.php
@@ -0,0 +1,77 @@
+<?php
+
+	namespace Ehven\Gilad\WordPress\Themes\Starters\TypeSix;
+
+    if ( ! defined( 'ABSPATH' ) ) exit( 'Nothing to see here. Go <a href="/">home</a>.' );
+
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<header class="entry-header">
+
+		<?php
+
+			if ( is_singular() ) {
+
+				the_title( '<h1 class="entry-title">', '</h1>' );
+
+			} else {
+
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			}
+
+			if ( 'post' === get_post_type() ) {
+
+				?>
+				<div class="entry-meta">
+
+					<?php
+
+						t_posted_on();
+
+						t_posted_by();
+
+					?>
+
+				</div><?php
+
+			}
+
+		?>
+
+	</header>
+
+	<?php t_post_thumbnail(); ?>
+
+	<div class="entry-content">
+
+		<?php
+
+			the_content( sprintf(
+				wp_kses(
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', '_s' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			) );
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
+				'after'  => '</div>',
+			) );
+
+		?>
+
+	</div>
+
+	<footer class="entry-footer">
+
+		<?php t_entry_meta(); ?>
+
+	</footer>
+
+</article>
