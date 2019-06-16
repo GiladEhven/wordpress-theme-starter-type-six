d00358a3f525a6e421c6443bbd752aea3fc46885
Gilad Ehven <gilad@ehven.com>
2019-06-16 07:46:12 -0400

    Implemented commenting system.

--- comments.php
+++ comments.php
@@ -6,20 +6,88 @@
 
     if ( post_password_required() ) return;
 
+?>
+
+<div id="comments" class="comments-area">
+
+    <?php
+
+        if ( have_comments() ) {
+
+            ?>
+
+            <h2>
+
+                <?php
+
+                    $v_comment_count = get_comments_number();
+
+                    // WPCS: XSS OK.
+                    printf( esc_html( _nx( TYPESIX_SET_COMMENTS_TITLE_FOR_ONE, TYPESIX_SET_COMMENTS_TITLE_FOR_MORE, $v_comment_count, 'comments title', 'wordpress-theme-starter-type-six' ) ), number_format_i18n( $v_comment_count ), '<span>' . get_the_title() . '</span>' );
+
+                ?>
+
+            </h2>
+
+            <?php the_comments_navigation(); ?>
+
+            <<?php echo TYPESIX_SET_COMMENTS_STYLE; ?>>
+
+                <?php
+
+                    wp_list_comments( array(
+
+                        'avatar_size'      => TYPESIX_SET_COMMENTS_AVATAR_SIZE,
+                        'reply_text'       => TYPESIX_SET_COMMENTS_REPLY_TEXT,
+                        'reverse_children' => TYPESIX_SET_COMMENTS_REVERSE_CHILDREN,
+                        'style'            => TYPESIX_SET_COMMENTS_STYLE,
+                        'type'             => TYPESIX_SET_COMMENTS_TYPE,
+
+                    ) );
+
+                ?>
+
+            </<?php echo TYPESIX_SET_COMMENTS_STYLE; ?>>
+
+            <?php
+
+                the_comments_navigation();
+
+                if ( ! comments_open() ) {
+
+                    ?><p class="no-comments"><?php esc_html_e( TYPESIX_SET_COMMENTS_CLOSED, 'wordpress-theme-starter-type-six' ); ?></p><?php
+                }
+
+        } else {
+
+            //
+
+        }
+
+        comment_form( array(
+
+            'comment_field'        => TYPESIX_SET_COMMENT_FORM_FIELD_COMMENT,
+            'comment_notes_after'  => TYPESIX_SET_COMMENT_FORM_NOTES_AFTER_BOX,
+            'comment_notes_before' => TYPESIX_SET_COMMENT_FORM_NOTES_BEFORE_BOX,
+
+            'fields'               => array(
+
+                'author'           => TYPESIX_SET_COMMENT_FORM_FIELD_AUTHOR,
+                'email'            => TYPESIX_SET_COMMENT_FORM_FIELD_EMAIL,
+                'url'              => TYPESIX_SET_COMMENT_FORM_FIELD_URL,
+
+            ),
+
+            'label_submit'         => TYPESIX_SET_COMMENT_FORM_SUBMIT,
+            'logged_in_as'         => TYPESIX_SET_COMMENT_FORM_LOGGED_IN_AS,
+            'must_log_in'          => TYPESIX_SET_COMMENT_FORM_MUST_LOG_IN,
+            'title_reply'          => TYPESIX_SET_COMMENT_FORM_TITLE,
+            'title_reply_after'    => TYPESIX_SET_COMMENT_FORM_REPLY_AFTER,
+            'title_reply_before'   => TYPESIX_SET_COMMENT_FORM_REPLY_BEFORE,
+            'title_reply_to'       => TYPESIX_SET_COMMENT_FORM_REPLY_TO,
+
+        ) );
+
     ?>
 
-                <section id="comments"><?php
-
-                    if ( have_comments() ) {
-
-                        // $template_comment = new Template_Comment();
-
-                    } else {
-
-                        //
-
-                    }
-
-                    comment_form(); ?>
-
-                </section>
+</div>
--- functions.php
+++ functions.php
@@ -6,6 +6,10 @@
 
 
 
+    $wp_user_object = get_userdata( get_current_user_id() );
+    $user_nicename  = $wp_user_object->user_nicename;
+
+
     if ( ! defined( 'TYPESIX_CSS_CLASSES_FOR_BODY'                  ) ) { define( 'TYPESIX_CSS_CLASSES_FOR_BODY',                  'default' );  }
     if ( ! defined( 'TYPESIX_CSS_CLASSES_FOR_BODY_LINER'            ) ) { define( 'TYPESIX_CSS_CLASSES_FOR_BODY_LINER',            'default' );  }
     if ( ! defined( 'TYPESIX_CSS_CLASSES_FOR_BODY_HEADER'           ) ) { define( 'TYPESIX_CSS_CLASSES_FOR_BODY_HEADER',           'default' );  }
@@ -28,6 +32,34 @@
 
 
     if ( ! defined( 'ENABLE_PALETTE'                                ) ) { define( 'ENABLE_PALETTE',                                false ); }
+
+
+
+    if ( ! defined( 'TYPESIX_SET_COMMENT_FORM_CANCEL_REPLY_AFTER'   ) ) { define( 'TYPESIX_SET_COMMENT_FORM_CANCEL_REPLY_AFTER',   '</small>' ); }
+    if ( ! defined( 'TYPESIX_SET_COMMENT_FORM_CANCEL_REPLY_BEFORE'  ) ) { define( 'TYPESIX_SET_COMMENT_FORM_CANCEL_REPLY_BEFORE',  '<small>' ); }
+    if ( ! defined( 'TYPESIX_SET_COMMENT_FORM_CANCEL_REPLY_LINK'    ) ) { define( 'TYPESIX_SET_COMMENT_FORM_CANCEL_REPLY_LINK',    __( 'Cancel reply' ) ); }
+    if ( ! defined( 'TYPESIX_SET_COMMENT_FORM_FIELD_AUTHOR'         ) ) { define( 'TYPESIX_SET_COMMENT_FORM_FIELD_AUTHOR',         '<p class="comment-form-author"><label for="author">' . __( ( $req ? '<strong>' : '' ) . 'Nameee'  . ( $req ? '</strong>' : '' ), 'wordpress-theme-starter-type-six' ) . '</label>' . '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' /></p>' ); }
+    if ( ! defined( 'TYPESIX_SET_COMMENT_FORM_FIELD_COMMENT'        ) ) { define( 'TYPESIX_SET_COMMENT_FORM_FIELD_COMMENT',        '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>' ); }
+    if ( ! defined( 'TYPESIX_SET_COMMENT_FORM_FIELD_EMAIL'          ) ) { define( 'TYPESIX_SET_COMMENT_FORM_FIELD_EMAIL',          '<p class="comment-form-email"><label for="email">'   . __( ( $req ? '<strong>' : '' ) . 'Emailll' . ( $req ? '</strong>' : '' ), 'wordpress-theme-starter-type-six' ) . '</label>' . '<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' /></p>' ); }
+    if ( ! defined( 'TYPESIX_SET_COMMENT_FORM_FIELD_URL'            ) ) { define( 'TYPESIX_SET_COMMENT_FORM_FIELD_URL',            '<p class="comment-form-url"><label for="url">'       . __( 'Websiteee', 'wordpress-theme-starter-type-six' ) . '</label>' . '<input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" /></p>' ); }
+    if ( ! defined( 'TYPESIX_SET_COMMENT_FORM_LOGGED_IN_AS'         ) ) { define( 'TYPESIX_SET_COMMENT_FORM_LOGGED_IN_AS',         '<p class="logged-in-as">' . sprintf( __( 'Logged in as <a href="%1$s">%2$s</a>. <a href="%3$s" title="Log out of this account">Log out?</a>' ), admin_url( 'profile.php' ), $user_nicename, wp_logout_url( apply_filters( 'the_permalink', get_permalink( ) ) ) ) . '</p>' ); }
+    if ( ! defined( 'TYPESIX_SET_COMMENT_FORM_MUST_LOG_IN'          ) ) { define( 'TYPESIX_SET_COMMENT_FORM_MUST_LOG_IN',          '<p class="must-log-in">' .  sprintf( __( 'You must be <a href="%s">logged in</a> to post a comment.' ), wp_login_url( apply_filters( 'the_permalink', get_permalink( ) ) ) ) . '</p>' ); }
+    if ( ! defined( 'TYPESIX_SET_COMMENT_FORM_NOTES_AFTER_BOX'      ) ) { define( 'TYPESIX_SET_COMMENT_FORM_NOTES_AFTER_BOX',      '<p class="form-allowed-tags">' . sprintf( __( 'You may use these HTML tags and attributes: %s' ), ' <code>' . allowed_tags() . '</code>' ) . '</p>' ); }
+    if ( ! defined( 'TYPESIX_SET_COMMENT_FORM_NOTES_BEFORE_BOX'     ) ) { define( 'TYPESIX_SET_COMMENT_FORM_NOTES_BEFORE_BOX',     '<p>Your email address will not be published.</p><p>Required fields are labeled in <strong>bold</strong></p>' ); }
+    if ( ! defined( 'TYPESIX_SET_COMMENT_FORM_REPLY_AFTER'          ) ) { define( 'TYPESIX_SET_COMMENT_FORM_REPLY_AFTER',          '</h3>' ); }
+    if ( ! defined( 'TYPESIX_SET_COMMENT_FORM_REPLY_BEFORE'         ) ) { define( 'TYPESIX_SET_COMMENT_FORM_REPLY_BEFORE',         '<h3 id="reply-title" class="comment-reply-title">' ); }
+    if ( ! defined( 'TYPESIX_SET_COMMENT_FORM_REPLY_TO'             ) ) { define( 'TYPESIX_SET_COMMENT_FORM_REPLY_TO',             __( 'Reply to %s' ) ); }
+    if ( ! defined( 'TYPESIX_SET_COMMENT_FORM_SUBMIT'               ) ) { define( 'TYPESIX_SET_COMMENT_FORM_SUBMIT',               __( 'Submit Comment' ) ); }
+    if ( ! defined( 'TYPESIX_SET_COMMENT_FORM_TITLE'                ) ) { define( 'TYPESIX_SET_COMMENT_FORM_TITLE',                __( 'Something to say?' ) ); }
+
+    if ( ! defined( 'TYPESIX_SET_COMMENTS_AVATAR_SIZE'              ) ) { define( 'TYPESIX_SET_COMMENTS_AVATAR_SIZE',              64 ); }
+    if ( ! defined( 'TYPESIX_SET_COMMENTS_CLOSED'                   ) ) { define( 'TYPESIX_SET_COMMENTS_CLOSED',                   'Comments are closed.' ); }
+    if ( ! defined( 'TYPESIX_SET_COMMENTS_REPLY_TEXT'               ) ) { define( 'TYPESIX_SET_COMMENTS_REPLY_TEXT',               'Reply to this comment' ); }
+    if ( ! defined( 'TYPESIX_SET_COMMENTS_REVERSE_CHILDREN'         ) ) { define( 'TYPESIX_SET_COMMENTS_REVERSE_CHILDREN',         false ); }
+    if ( ! defined( 'TYPESIX_SET_COMMENTS_TITLE_FOR_MORE'           ) ) { define( 'TYPESIX_SET_COMMENTS_TITLE_FOR_MORE',           '%1$s comments on &ldquo;%2$s&rdquo;' ); }
+    if ( ! defined( 'TYPESIX_SET_COMMENTS_TITLE_FOR_ONE'            ) ) { define( 'TYPESIX_SET_COMMENTS_TITLE_FOR_ONE',            'One comment on &ldquo;%2$s&rdquo;' ); }
+    if ( ! defined( 'TYPESIX_SET_COMMENTS_STYLE'                    ) ) { define( 'TYPESIX_SET_COMMENTS_STYLE',                    'ol' ); }
+    if ( ! defined( 'TYPESIX_SET_COMMENTS_TYPE'                     ) ) { define( 'TYPESIX_SET_COMMENTS_TYPE',                     'comment' ); }
 
 
 
