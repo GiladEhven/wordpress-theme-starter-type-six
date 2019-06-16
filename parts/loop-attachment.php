9fb16d4b0532c15b0887b0fac4c4bb474c8414a7
Gilad Ehven <gilad@ehven.com>
2019-06-15 22:00:31 -0400

    Updated namespacing.

--- parts/loop-attachment.php
+++ parts/loop-attachment.php
@@ -1,5 +1,5 @@
 <?php
 
-	namespace Ehven\Gilad\WordPress\Themes\Starters\TypeSix;
+	namespace Ehven\Gilad\WordPress\Themes\Parents\TypeSix;
 
     if ( ! defined( 'ABSPATH' ) ) exit( 'Nothing to see here. Go <a href="/">home</a>.' );
--- support/public-bootstrap/version-4.1.3.php
+++ support/public-bootstrap/version-4.1.3.php
@@ -1,6 +1,6 @@
 <?php
 
-	namespace Ehven\Gilad\WordPress\Themes\Starters\TypeSix;
+	namespace Ehven\Gilad\WordPress\Themes\Parents\TypeSix;
 
     if ( ! defined( 'ABSPATH' ) ) exit( 'Nothing to see here. Go <a href="/">home</a>.' );
 
